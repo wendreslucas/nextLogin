747b4709b6417528dc80339b03a80ee6cfceb55c
Wendres Lucas <wendreslucas@gmail.com>
2022-08-23 11:04:41 -0300

    implements cookies with nookies lib

--- pages/auth-page-srr.jsx
+++ pages/auth-page-srr.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { tokenService } from "./../src/service/auth/tokenService";
+import nookies from "nookies";
+
+export async function getServerSideProps(ctx) {
+  console.log(tokenService.get());
+  const cookies = nookies.get(ctx);
+  console.log(cookies);
+
+  return {
+    props: {
+      tokenService: tokenService.get(ctx),
+    },
+  };
+}
 
 function AuthPageSrr(props) {
-  console.log(tokenService.get());
-
   return (
     <div>
       <h1>Auth Page Server Side Rendering</h1>
--- src/service/auth/tokenService.js
+++ src/service/auth/tokenService.js
@@ -1,16 +1,30 @@
 const ACCESS_TOKEN = "ACCESS_TOKEN";
+import nookies from "nookies";
+
+const ONE_SECOND = 1;
+const ONE_MINUTE = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
+const ONE_YEAR = ONE_DAY * 365;
 
 export const tokenService = {
-  save(accessToken) {
+  save(accessToken, ctx = null) {
     globalThis?.localStorage?.setItem(ACCESS_TOKEN, accessToken);
     globalThis?.sessionStorage?.setItem(ACCESS_TOKEN, accessToken);
+    nookies.set(ctx, ACCESS_TOKEN, accessToken, {
+      maxAge: ONE_YEAR,
+      path: "/",
+    });
   },
-  get() {
-    return globalThis?.localStorage?.getItem(ACCESS_TOKEN);
+  get(ctx = null) {
+    //return globalThis?.localStorage?.getItem(ACCESS_TOKEN);
     //return sessionStorage.getItem(ACCESS_TOKEN);
+    const cookies = nookies.get(ctx);
+    return cookies[ACCESS_TOKEN] || "";
   },
   delete() {
     globalThis?.localStorage?.removeItem(ACCESS_TOKEN);
     globalThis?.sessionStorage?.removeItem(ACCESS_TOKEN);
+    nookies.destroy(ctx, ACCESS_TOKEN);
   },
 };
