1c45bf95499c700e190017e3bba3913341de1fc9
Wendres Lucas <wendreslucas@gmail.com>
2022-08-23 16:10:29 -0300

    implements High order component

--- pages/auth-page-static.jsx
+++ pages/auth-page-static.jsx
@@ -1,50 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { authService } from "./../src/service/auth/authService";
-import { useRouter } from "next/router";
-
-function useSession() {
-  const [session, setSession] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    authService
-      .getSession()
-      .then((session) => {
-        setSession(session);
-        console.log(session);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  return {
-    data: {
-      session,
-    },
-    error,
-    loading,
-  };
-}
+import React from "react";
+import { withSessionHOC } from "./../src/service/auth/session";
 
 function AuthPageStatic(props) {
-  const router = useRouter();
-  const session = useSession();
-
-  if (!session.loading && session.error) {
-    console.log("redireciona");
-    router.push("/?error=401");
-  }
   return (
     <div>
       <h1>Auth Page Static</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
     </div>
   );
 }
 
-export default AuthPageStatic;
+export default withSessionHOC(AuthPageStatic);
--- src/service/auth/session.js
+++ src/service/auth/session.js
@@ -1,4 +1,7 @@
 import { authService } from "./authService";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export function withSession(funcao) {
   return async (ctx) => {
@@ -20,5 +23,52 @@ export function withSession(funcao) {
         },
       };
     }
+  };
+}
+
+export function useSession() {
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    authService
+      .getSession()
+      .then((session) => {
+        setSession(session);
+        console.log(session);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    data: {
+      session,
+    },
+    error,
+    loading,
+  };
+}
+
+export function withSessionHOC(Component) {
+  return function Wrapper(props) {
+    const router = useRouter();
+    const session = useSession();
+
+    if (!session.loading && session.error) {
+      console.log("redireciona");
+      router.push("/?error=401");
+    }
+    const modifiedProps = {
+      ...props,
+      session: session.data.session,
+    };
+
+    return <Component {...modifiedProps} />;
   };
 }
