b11b86d5744a7a6260842da2af6009451cb72927
Wendres Lucas <wendreslucas@gmail.com>
2022-08-24 14:58:36 -0300

    begin implements refresh token

--- src/service/auth/authService.js
+++ src/service/auth/authService.js
@@ -6,12 +6,18 @@ export const authService = {
     return HttpClient(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
       method: "POST",
       body: { username, password },
-    }).then(async (respostaDoServidor) => {
-      if (!respostaDoServidor.ok) throw new Error("Dados incorretos");
-      const body = respostaDoServidor.body;
+    })
+      .then(async (respostaDoServidor) => {
+        if (!respostaDoServidor.ok) throw new Error("Dados incorretos");
+        const body = respostaDoServidor.body;
 
-      tokenService.save(body.data.access_token);
-    });
+        tokenService.save(body.data.access_token);
+        return body;
+      })
+      .then(({ data }) => {
+        const { refresh_token } = data;
+        console.log("Refresh Token: " + refresh_token);
+      });
   },
   async getSession(ctx = null) {
     const token = tokenService.get(ctx);
